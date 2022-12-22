0d18d473c5cee3ec55e64e3d6a9204dbc8732b34
wendy <wendyz34@nycstudents.net>
2022-12-21 20:12:18 -0500

    Initial commit

---
+++ src/TreasureHunterRunner.java
@@ -0,0 +1,10 @@
+
+    public class TreasureHunterRunner
+    {
+        public static void main(String args[])
+        {
+            TreasureHunter game = new TreasureHunter();
+            game.play();
+        }
+    }
+
